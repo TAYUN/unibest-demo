25812a3018b26ae8da49a924ee6f74b3d17d8b51
weiye <507028106@qq.com>
2025-04-03 16:09:49 +0800

    feat(wot): 添加表单校验问题页面
    - 在 wot 页面中添加了一个新的按钮，用于导航到表单校验问题页面
    - 该按钮位于其他 issue 按钮下方，用于测试和演示表单 v-if 校验问题

--- src/pages/wot/wot.vue
+++ src/pages/wot/wot.vue
@@ -13,6 +13,7 @@
     <button @click="navTo('/pages/wot/issue/borderNoShow')">第一个cell border线不显示</button>
     <button @click="navTo('/pages/wot/issue/wdTabsIssue')">wd-tabs吸顶问题</button>
     <button @click="navTo('/pages/wot/issue/pickerViewIssue')">pickerView无法滚动</button>
+    <button @click="navTo('/pages/wot/issue/formIssue')">表单v-if校验问题</button>
   </view>
 </template>
 
